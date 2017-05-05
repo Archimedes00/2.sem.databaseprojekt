211e3c68bcbf377b5871a46df283ddeb63b8a796
Stuart <Stuart@192.168.0.10>
2017-05-05 22:33:28 +0200

    t

--- Documents/workspace1/2.sem-databaseprojekt/src/test01917/TUI.java
+++ Documents/workspace1/2.sem-databaseprojekt/src/test01917/TUI.java
@@ -151,50 +151,7 @@ public class TUI implements ITUI
         }
         
         scan.close();
-        
-        
-    
-        /*
-		System.out.println("Operatoer nummer 3:");
-		MySQLOperatoerDAO opr = new MySQLOperatoerDAO();
-		try { System.out.println(opr.getOperatoer(3)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-
-		System.out.println("Indsaettelse af ny operatoer med opr_id =  4");
-		OperatoerDTO oprDTO = new OperatoerDTO(4,"Don Juan","DJ","000000-0000","iloveyou");
-		try { opr.createOperatoer(oprDTO); }
-		catch (DALException e) { System.out.println(e.getMessage()); }	
-
-		System.out.println("Operatoer nummer 4:");
-		try { System.out.println(opr.getOperatoer(4)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-
-		System.out.println("Opdatering af initialer for operatoer nummer 4");
-		oprDTO.setIni("DoJu");
-		try { opr.updateOperatoer(oprDTO); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-
-		System.out.println("Operatoer nummer 4:");
-		try { System.out.println(opr.getOperatoer(4)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-
-		System.out.println("Alle operatoerer:");
-		try { System.out.println(opr.getOperatoerList()); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-
-		System.out.println("Operatoer nummer 5:");
-		try { System.out.println(opr.getOperatoer(5)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }		
-
-		System.out.println("Raavare nummer 3:");
-		RaavareDAO raavare = new MySQLRaavareDAO();
-		try { System.out.println(raavare.getRaavare(3)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		
-		System.out.println("Raavare nummer 6:");
-		try { System.out.println(raavare.getRaavare(6)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-		*/
+      
 	}
 
 	
