0437731dec7b216189b7ec02beb129f724fb67ba
bijannegari <bijannegari@10.182.16.13>
2017-05-05 19:46:47 +0200

    k

--- Documents/workspace1/2.sem-databaseprojekt/src/test01917/Main.java
+++ Documents/workspace1/2.sem-databaseprojekt/src/test01917/Main.java
@@ -29,11 +29,11 @@ public class Main {
 		
 		MySQLOperatoerDAO opr = new MySQLOperatoerDAO();
 		MySQLProduktBatchDAO produktbatch = new MySQLProduktBatchDAO();
-		MySQLProduktBatchKompDAO produktbatchKomp = new MySQLProduktBatchKompDAO();
-		MySQLRaavareBatchDAO raavareBatch = new MySQLRaavareBatchDAO();
+		MySQLProduktBatchKompDAO produktbatchkomp = new MySQLProduktBatchKompDAO();
+		MySQLRaavareBatchDAO raavarebatch = new MySQLRaavareBatchDAO();
 		MySQLRaavareDAO raavare = new MySQLRaavareDAO();
 		MySQLReceptDAO recept = new MySQLReceptDAO();
-		MySQLReceptKompDAO receptKomp = new MySQLReceptKompDAO();
+		MySQLReceptKompDAO receptkomp = new MySQLReceptKompDAO();
 		
 		
 		
@@ -47,18 +47,15 @@ public class Main {
 		OperatoerDTO oprDTO = new OperatoerDTO(4,"Don Juan","DJ","000000-0000","iloveyou",1);
 		try { opr.createOperatoer(oprDTO); }
 		catch (DALException e) { System.out.println(e.getMessage()); }	
-
-		System.out.println("Operatoer nummer 4:");
-		try { System.out.println(opr.getOperatoer(4)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
-
+		
 		System.out.println("Opdatering af initialer for operatoer nummer 4");
 		oprDTO.setIni("DoJu");
 		try { opr.updateOperatoer(oprDTO); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
-
-		System.out.println("Operatoer nummer 4:");
-		try { System.out.println(opr.getOperatoer(4)); }
+		
+		System.out.println("Indsaettelse af ny operatoer med opr_id =  5");
+		OperatoerDTO oprDTO2 = new OperatoerDTO(5,"Bij Kapish","BK","123456-1337","sutmig",1);
+		try { opr.createOperatoer(oprDTO2); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
 
 		System.out.println("Alle operatoerer:");
@@ -74,15 +71,22 @@ public class Main {
 		catch (DALException e) { System.out.println(e.getMessage()); }		
 
 		
+		System.out.println("Produktbatch nummer 2");
+		try{ System.out.println((produktbatch.getProduktBatch(2))); }
+		catch (DALException e) { System.out.println(e.getMessage());}
 		
-		
-		
-		System.out.println("Raavare nummer 3:");
-		try { System.out.println(raavare.getRaavare(3)); }
+		System.out.println("Indsaettelse af ny produktbatch med pb_id =  7");
+		ProduktBatchDTO produktbatchDTO = new ProduktBatchDTO(7, 0, 4);
+		try { produktbatch.createProduktBatch(produktbatchDTO); }
 		catch (DALException e) { System.out.println(e.getMessage()); }
 		
-		System.out.println("Raavare nummer 10:");
-		try { System.out.println(raavare.getRaavare(10)); }
-		catch (DALException e) { System.out.println(e.getMessage()); }
+		
+//		System.out.println("Raavare nummer 3:");
+//		try { System.out.println(raavare.getRaavare(3)); }
+//		catch (DALException e) { System.out.println(e.getMessage()); }
+//		
+//		System.out.println("Raavare nummer 10:");
+//		try { System.out.println(raavare.getRaavare(10)); }
+//		catch (DALException e) { System.out.println(e.getMessage()); }
 	}
 }
