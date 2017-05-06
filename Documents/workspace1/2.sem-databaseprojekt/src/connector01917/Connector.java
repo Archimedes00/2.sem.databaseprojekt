ac219e6dd8ed6d2e19469f2c57417484286f383a
Stuart <Stuart@192.168.0.10>
2017-05-06 19:41:16 +0200

    tis

--- Documents/workspace1/2.sem-databaseprojekt/src/connector01917/Connector.java
+++ Documents/workspace1/2.sem-databaseprojekt/src/connector01917/Connector.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-/** @author Ronnie Dalsgaard */
+/** @author Ronie Dalsgaard */
 public class Connector {
     private  String HOST;
     private  int PORT;
