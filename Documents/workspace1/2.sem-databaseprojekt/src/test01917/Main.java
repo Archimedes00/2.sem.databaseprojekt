22879b9572d5e0a1e807b12f7ed6f04655ae9d22
Stuart <Stuart@192.168.0.10>
2017-05-06 18:34:59 +0200

    t

--- Documents/workspace1/2.sem-databaseprojekt/src/test01917/Main.java
+++ Documents/workspace1/2.sem-databaseprojekt/src/test01917/Main.java
@@ -1,28 +1,4 @@
-package test01917;import daoimpl01917.MySQLProduktBatchDAO;
-import daoimpl01917.MySQLProduktBatchKompDAO;
-import daoimpl01917.MySQLRaavareBatchDAO;
-import daoimpl01917.MySQLRaavareDAO;
-import daoimpl01917.MySQLReceptDAO;
-import daoimpl01917.MySQLReceptKompDAO;
-import daointerfaces01917.DALException;
-import daointerfaces01917.ProduktBatchDAO;
-import daointerfaces01917.ProduktBatchKompDAO;
-import daointerfaces01917.RaavareBatchDAO;
-import daointerfaces01917.RaavareDAO;
-import daointerfaces01917.ReceptDAO;
-import daointerfaces01917.ReceptKompDAO;
-import daointerfaces01917.UserDAO;
-import dto01917.ProduktBatchDTO;
-import dto01917.ProduktBatchKompDTO;
-import dto01917.RaavareBatchDTO;
-import dto01917.RaavareDTO;
-import dto01917.ReceptDTO;
-import dto01917.ReceptKompDTO;
-import dto01917.UserDTO;
-import java.sql.SQLException;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-import connector01917.Connector;
+package test01917;
 
 public class Main 
 {
