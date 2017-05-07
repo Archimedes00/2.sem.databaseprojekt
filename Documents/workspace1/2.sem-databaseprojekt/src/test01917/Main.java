c6b0697da0f8614f430c869347415fd8d34dd6f5
LAPTOP-2KGNLENP\Thoma <Thomaslienchristensen@gmail.com>
2017-05-07 16:17:32 +0200

    Påbegyndt JUNIT test

--- Documents/workspace1/2.sem-databaseprojekt/src/test01917/Main.java
+++ Documents/workspace1/2.sem-databaseprojekt/src/test01917/Main.java
@@ -1,10 +1,21 @@
 package test01917;
 
+import java.sql.SQLException;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
-		ITUI userInterface = new TUI();
-		userInterface.Selector();
+//		ITUI userInterface = new TUI();
+//		userInterface.Selector();
+		
+		JUnitDBTest test = new JUnitDBTest();
+		try {
+		test.opretOperator();
+		System.out.println("Bruger oprettet");
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
