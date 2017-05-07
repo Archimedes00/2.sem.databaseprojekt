72b4a3a4d6c911827277f16538dad28592c817e9
bijannegari <bijannegari@10.182.16.13>
2017-05-07 18:26:19 +0200

    tutt

--- Documents/workspace1/2.sem-databaseprojekt/src/test01917/Main.java
+++ Documents/workspace1/2.sem-databaseprojekt/src/test01917/Main.java
@@ -6,16 +6,16 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-//		ITUI userInterface = new TUI();
-//		userInterface.Selector();
+		ITUI userInterface = new TUI();
+		userInterface.Selector();
 		
-		JUnitDBTest test = new JUnitDBTest();
-		try {
-		test.opretOperator();
-		System.out.println("Bruger oprettet");
-		}
-		catch(Exception ex) {
-			ex.printStackTrace();
-		}
+//		JUnitDBTest test = new JUnitDBTest();
+//		try {
+//		test.opretOperator();
+//		System.out.println("Bruger oprettet");
+//		}
+//		catch(Exception ex) {
+//			ex.printStackTrace();
+//		}
 	}
 }
